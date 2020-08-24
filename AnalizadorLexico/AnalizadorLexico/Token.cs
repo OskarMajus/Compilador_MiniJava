1a1e1e37cc0ed2f6d65538eaac8b39e45cf0f614
Oskar <oskarmajus@gmail.com>
2020-08-24 17:19:02 -0600

    Creación clase token

---
+++ AnalizadorLexico/AnalizadorLexico/Token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico
+{
+    class Token
+    {
+        public Token(string nombre, string lexema, int linea, int columna) 
+        {
+            Nombre = nombre;
+            Lexema = lexema;
+            Linea = linea;
+            Columna = columna;
+        }
+
+        public string Nombre { get; set; }
+
+        public string Lexema { get; private set; }
+
+        public int Linea { get; private set; }
+
+        public int Columna { get;  private set; }
+    }
+}
