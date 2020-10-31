0fde04c36e9f6316ea95c3d7e39a86b6dfd0ddd7
Oskar <oskarmajus@gmail.com>
2020-10-31 00:33:46 -0600

    Creación de clase Analizador

---
+++ AnalizadorLexico/AnalizadorLexico/Analizador_Sintactico/Analizador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico.Analizador_Sintactico
+{
+    public class LALRPropagacion
+    {
+        //Estado Objetivo
+        public int LR0TargetState { get; set; }
+
+        public int LR0TargetItem { get; set; }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Analizador
+    {
+
+    }
+}
